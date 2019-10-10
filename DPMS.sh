9de42f467c97b02fe4e66a218420e782318a6a76
as3ii <as3ii777@gmail.com>
2019-10-10 16:24:57 +0200

    edited DPMS.sh script

--- DPMS.sh
+++ DPMS.sh
@@ -5,5 +5,5 @@ revert() {
 }
 trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
-./i3lock-multimonitor/lock -i /home/as3ii/Pictures/SteamPunk/IMG_534952.png 
+/home/as3ii/.script/i3lock-multimonitor/lock -i /home/as3ii/Pictures/SteamPunk/IMG_534952.png 
 revert
