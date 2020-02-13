72c01b77a53f36ba39dade2953ee7fbaa2df4ecf
as3ii <as3ii777@gmail.com>
2020-02-13 11:20:08 +0100

    added previewer for lf, edited lock and suspend scripts

--- DPMSS.sh
+++ DPMSS.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+systemctl suspend
 revert() {
     xset dpms 0 0 0
 }
@@ -7,3 +8,4 @@ trap revert HUP INT TERM
 xset +dpms dpms 5 5 5
 /home/as3ii/.script/i3lock-multimonitor/lock -i /home/as3ii/Pictures/SteamPunk/IMG_534952.png 
 revert
+
