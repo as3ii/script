fc63d4cfe8d185bb927bd3545ab54db351d6006c
as3ii <as3ii777@gmail.com>
2020-03-19 02:30:01 +0100

    updated margin size in pandoc-pdf script

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -2,5 +2,5 @@
 
 cd "${1%/*}" || exit
 
-pandoc "$1" -H ~/.script/header.tex -f markdown -t pdf --toc -V geometry:margin=1in | zathura - &
+pandoc "$1" -H ~/.script/header.tex -f markdown -t pdf --toc -V geometry:margin=0.8in | zathura - &
 
