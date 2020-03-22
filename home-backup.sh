eceff9884228322ca26b2dfd7d881df903a97d60
as3ii <as3ii777@gmail.com>
2020-03-22 18:34:51 +0100

    updated home-backup script

--- home-backup.sh
+++ home-backup.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 printf "starting home backup...\n"
-sources=$(printf "%s" "$(find . -maxdepth 1 -name "*" -not -path "*.cache*" -not -path "." -not -path "*.nvm*" -printf "%f\n")")
-for line in $sources; do
-    printf "backupping: %s\n" "$line"
-    rsync -a -hh --delete --info=stats1 --info=progress2 --modify-window=1 "$line" /mnt/volume/Doc/home-backup
-    sleep 1
-done
+rsync -a --delete -hh --info=stats1 --info=progress2 --modify-window=1 --exclude=.cache --exclude=.nvm /home/as3ii/ /mnt/volume/Doc/home-backup
 printf "home backup ended.\nStart syncing...\n"
 sync -f /mnt/volume/Doc/home-backup
 printf "Done!\n"
