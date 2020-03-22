1f03372653e76caa0da3e107c96f4be2284f03c1
as3ii <as3ii777@gmail.com>
2020-03-22 20:56:27 +0100

    improoved dmenu_calc script (thanks @ShellAddicted)

--- dmenu_calc.sh
+++ dmenu_calc.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
 eq=$(printf "" | dmenu -fn "Monospace 10" -p "write equation: ")
-ret=$(python3 -c "import math; print($eq)")
-notify-send -u low -t 5000 "Result" "$ret" 
+printf "%s" "$eq"
+if [ "$eq" != "" ]; then
+    ret=$(python3 -c "
+from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+from io import BytesIO
+from decimal import Decimal, getcontext
+getcontext().prec = 6
+
+def float_sucks(s):
+    # Stolen from: https://docs.python.org/3.7/library/tokenize.html#examples
+    result = []
+    g = tokenize(BytesIO(s.encode('utf-8')).readline)  # tokenize the string
+    for toknum, tokval, _, _, _ in g:
+        if toknum == NUMBER:  # replace NUMBER tokens
+            result.extend([
+                (NAME, 'Decimal'),
+                (OP, '('),
+                (STRING, repr(tokval)),
+                (OP, ')')
+            ])
+        else:
+            result.append((toknum, tokval))
+    return untokenize(result).decode('utf-8')
+
+
+res = float_sucks(\"$eq\")
+exec(f'print({res})')
+")
+    notify-send -u low -t 5000 "Result" "$ret";
+fi
+
