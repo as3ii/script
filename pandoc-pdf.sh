6a4eccdfcc864a36dd71f951423c75ec79d46a0c
as3ii <as3ii777@gmail.com>
2020-04-17 17:50:02 +0200

    improved markdown to pdf script previewer

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cd "${1%/*}" || exit
-
+TMPP="/tmp/zathura-preview-$PPID.pdf"
+if [ -f $TMPP ]; then
+	kill "$(cat $TMPP)" 2>/dev/null
+	rm $TMPP
+fi
 pandoc "$1" -H ~/.script/header.tex -f markdown -t pdf --toc -V geometry:margin=0.8in | zathura - &
+PID=$!
+printf "%s" "$PID" > $TMPP
 
