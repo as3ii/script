d998f269ce37e22d44b261911b18b4cf97762790
as3ii <as3ii777@gmail.com>
2020-04-22 16:00:28 +0200

    added option to save compiled pdf, added option to merge multiple .md files into a single pdf

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
 TMPP="/tmp/zathura-preview-$PPID.pdf"
-pandoc "$1" -H ~/.script/header.tex -f markdown -t pdf --toc -V geometry:margin=0.8in -o $TMPP
+
+EXPORT=""
+
+if [ "$1" = "--export" ] || [ "$1" = "-e" ]; then
+	shift
+	if [ -n "$2" ]; then
+		EXPORT="$1"
+		shift
+	else
+		printf "Usage: %s [-e filename.pdf] file.md [file2.md [...]]" "$0"
+		exit 1
+	fi
+fi
+
+pandoc "$@" -H ~/.script/header.tex -f markdown -t pdf --toc -V geometry:margin=0.8in -o $TMPP
+
+if [ -n "$EXPORT" ]; then
+	cp "$TMPP" "$EXPORT"
+fi
+
 if ! pgrep -f "zathura $TMPP"; then
     (zathura $TMPP && rm $TMPP) &
 fi
