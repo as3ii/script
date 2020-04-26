92dae4ec78864762bbb10e72ca76b62ee9bc9e19
as3ii <as3ii777@gmail.com>
2020-04-26 19:19:44 +0200

    minor updates

--- home-backup.sh
+++ home-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf "starting home backup...\n"
-rsync -a --delete -hh --info=stats1 --info=progress2 --modify-window=1 --exclude=.cache --exclude=.nvm /home/as3ii/ /mnt/volume/Doc/home-backup
+rsync -a --delete -hh --info=stats1 --info=progress2 --modify-window=1 --exclude=.cache --exclude=.ccache --exclude=.nvm /home/as3ii/ /mnt/volume/Doc/home-backup
 printf "home backup ended.\nStart syncing...\n"
 sync -f /mnt/volume/Doc/home-backup
 printf "Done!\n"
