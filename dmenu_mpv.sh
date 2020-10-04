c3c7f51fb26e216671798120a4e13725d0165e88
as3ii <as3ii777@gmail.com>
2020-10-04 19:13:04 +0200

    added clipmenu to dmenu_mpv.sh

--- dmenu_mpv.sh
+++ dmenu_mpv.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-link="$(echo "$(xclip -o -sel clip)\n$(find . -L -type f -maxdepth 5 -name "*" ! -path "./.*" ! -path "." -printf "%f\n")" | dmenu -fn "Monospace 10" -p "Select copyed link: ")"
+
+if command -v clipmenu >/dev/null; then
+    link="$(CM_OUTPUT_CLIP=1 clipmenu -fn "Monospace 10" -p "Select copyed link: ")"
+else
+    link="$(echo "$(xclip -o -sel clip)\n\
+        $(find . -L -type f -maxdepth 5 -name "*" ! -path "./.*"\
+        ! -path "." -printf "%f\n")"\
+        | dmenu -fn "Monospace 10" -p "Select copyed link: ")"
+fi
+
 if [ -n "$link" ]; then
     mpv "$link" >/dev/null 2>&1
     case $? in
