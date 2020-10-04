aa952b66cba71f57bdd9e82314f4a6d222bc427c
as3ii <as3ii777@gmail.com>
2020-10-04 19:14:56 +0200

    added ionice to home-backup to manage priority

--- home-backup.sh
+++ home-backup.sh
@@ -4,10 +4,10 @@ BORG_REPO=/mnt/volume/Doc/home-borg
 
 if [ "$1" = "-c" ]; then
     printf "checking integrity...\n"
-    borg check -v $BORG_REPO || exit 1
+    ionice -c 3 borg check -v $BORG_REPO || exit 1
 fi
 printf "starting home backup...\n"
-borg create -s --progress -C auto,zstd,2 -x \
+ionice -c 3 borg create -s --progress -C auto,zstd,2 -x \
     -e *.cache* -e *.ccache* -e *.nvm* --exclude-caches \
     "$BORG_REPO::as3ii-{now}" /home/as3ii
 printf "home backup ended.\nStart syncing...\n"
