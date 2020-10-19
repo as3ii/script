14ff163acd8c5cdc955cafc7e941385c104c28f0
as3ii <as3ii777@gmail.com>
2020-10-20 00:54:57 +0200

    Massive rewrite and improvement of home-backup.sh

--- home-backup.sh
+++ home-backup.sh
@@ -1,17 +1,52 @@
 #!/bin/sh
 
-BORG_REPO=/mnt/volume/Doc/home-borg
-
-if [ "$1" = "-c" ]; then
-    printf "checking integrity...\n"
-    ionice -c 3 borg check -v $BORG_REPO || exit 1
+# check global variable
+if [ -z "$BORG_REPO" ]; then
+    export BORG_REPO="/mnt/archive/backup"
 fi
-printf "starting home backup...\n"
-ionice -c 3 borg create -s --progress -C auto,zstd,2 -x \
-    -e *.cache* -e *.ccache* -e *.nvm* --exclude-caches \
-    "$BORG_REPO::as3ii-{now}" /home/as3ii
-printf "home backup ended.\nStart syncing...\n"
+
+# check target folders
+if [ -z "${BORG_REPO##/home*}" ]; then
+    printf "Target folder could not be inside the home directory\n"
+    exit 1
+fi
+
+# check if the repo exists
+if ! [ "$(grep "Borg" "$BORG_REPO/README")" = \
+    "This is a Borg Backup repository." ]; then
+    borg init -e repokey-blake2 "$BORG_REPO"
+fi
+
+# check for 'check' flag
+if [ "$1" = "-c" ] || [ "$1" = "--check" ]; then
+    printf "Checking integrity...\n"
+    ionice -c 3 borg check -v "$BORG_REPO" || exit 1
+fi
+
+printf "Starting home backup...\n"
+ionice -c 3 borg create -s --progress -C auto,zstd,7 -x \
+    -e '/home/*/.cache/ccache' \
+    -e '/home/*/.cache/mozilla/firefox/*/cache*' \
+    -e '/home/*/.cache/spotify/Data' \
+    -e '/home/*/.cache/winetricks' \
+    -e '/home/*/.cache/keybase/*log*' \
+    -e '/home/*/.cache/wine' \
+    -e '/home/*/.cache/calibre' \
+    -e '/home/*/.cache/supertuxkart' \
+    -e '/home/*/.local/share/Steam' \
+    -e '/home/*/.local/share/Trash' \
+    -e '/home/*/.local/share/TelegramDesktop' \
+    -e '/home/*/.local/share/cargo' \
+    -e '/home/*/.nvm' \
+    -e '/home/*/.nv' \
+    --exclude-caches --show-rc \
+    ::'{hostname}-{now}' /home
+
+printf "Pruning repository...\n"
+ionice -c 3 borg prune -s --prefix '{hostname}-' \
+    --keep-daily 3 --show-rc
+
+printf "Starting sync...\n"
 sync -f $BORG_REPO
 printf "Done!\n"
-
 
