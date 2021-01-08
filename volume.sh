a5741835235dd7730183c0759c1da261baecb803
as3ii <as3ii777@gmail.com>
2021-01-08 15:10:52 +0100

    volume.sh: amixer now use mapped volume; added "master" mixer detection

--- volume.sh
+++ volume.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-amixer scontrols | grep "Master" && name="Master" || name="PCM"
+# find right mixer control to use
+name="$(amixer scontrols | sed "s/^.* '//;s/',[0-9]$//;/.*[Mm]ic.*/d;q")"
 
 case "$1" in
-    up) amixer sset "$name" 5%+ && notify-send -u low -t 500 'volume up';;
-    down) amixer sset "$name" 5%- && notify-send -u low -t 500 'volume down';;
+    up) amixer -M sset "$name" 5%+ && notify-send -u low -t 500 'volume up';;
+    down) amixer -M sset "$name" 5%- && notify-send -u low -t 500 'volume down';;
     toggle) amixer sset "$name" toggle && notify-send -u low -t 500 'muted/unmuted';;
     *) printf "error: %s not found" "$1"
 esac
