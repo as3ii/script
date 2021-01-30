57902858da9c0874e88f36eac4dd4319650a4d97
as3ii <as3ii777@gmail.com>
2021-01-30 16:41:07 +0100

    dmenu_rbw: added more checks, now clipmenu is not a dependency

--- dmenu_rbw.sh
+++ dmenu_rbw.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/sh
 
-rbw sync
-entry="$(rbw ls | dmenu -i -fn 'Monospace 10')"
-if command -v clipmenu >/dev/null; then
-    clipctl disable
-    printf "%s" "$(rbw get "$entry")" | xsel -ibn &
-    xsel_pid=$!
-    clipctl enable
-    notify-send -u normal -t 3000 "password stored in the clipboard, available only for 30 seconds"
-    sleep 30
-    timeout -k 3 2 kill -15 "$xsel_pid" || kill -9 "$xsel_pid"
+if command -v clipctl >/dev/null; then
+    _clipctl=true
 else
-    notify-send -u critical -t 5000 "clipmenu related binary not found"
+    _clipctl=false
 fi
+
+if command -v xsel >/dev/null; then
+    notify-send -u critical -t 5000 "xsel binary not found"
+    exit 1
+fi
+
+if command -v rbw >/dev/null; then
+    notify-send -u critical -t 5000 "rbw binary not found"
+    exit 1
+fi
+
+# sync database
+rbw sync
+# open dmenu with the list of accounts
+entry="$(rbw ls | dmenu -i -fn 'Monospace 10')"
+# stop clipmenu
+if $_clipctl; then clipctl disable; fi
+# copy password to the clipboard
+printf "%s" "$(rbw get "$entry")" | xsel -ibn &
+xsel_pid=$!
+# restart clipmenu
+if $_clipctl; then clipctl enable; fi
+notify-send -u normal -t 3000 "password stored in the clipboard, available only for 30 seconds"
+# kill xsel after 30 seconds
+sleep 30
+timeout -k 3 2 kill -15 "$xsel_pid" || kill -9 "$xsel_pid"
 
