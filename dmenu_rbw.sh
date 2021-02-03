04537b70fcd0142293f3e94141c85ae54a5d4787
as3ii <as3ii777@gmail.com>
2021-02-03 16:54:22 +0100

    dmenu_rbw: fixed command detection, fixed right exit code check for no selection from dmenu

--- dmenu_rbw.sh
+++ dmenu_rbw.sh
@@ -6,20 +6,23 @@ else
     _clipctl=false
 fi
 
-if command -v xsel >/dev/null; then
+if ! command -v xsel >/dev/null; then
     notify-send -u critical -t 5000 "xsel binary not found"
-    exit 1
+    exit 127
 fi
 
-if command -v rbw >/dev/null; then
+if ! command -v rbw >/dev/null; then
     notify-send -u critical -t 5000 "rbw binary not found"
-    exit 1
+    exit 127
 fi
 
 # sync database
 rbw sync
 # open dmenu with the list of accounts
 entry="$(rbw ls | dmenu -i -fn 'Monospace 10')"
+if [ -z "$entry" ]; then
+    exit 1
+fi
 # stop clipmenu
 if $_clipctl; then clipctl disable; fi
 # copy password to the clipboard
@@ -27,7 +30,8 @@ printf "%s" "$(rbw get "$entry")" | xsel -ibn &
 xsel_pid=$!
 # restart clipmenu
 if $_clipctl; then clipctl enable; fi
-notify-send -u normal -t 3000 "password stored in the clipboard, available only for 30 seconds"
+notify-send -u normal -t 3000 \
+    "password stored in the clipboard, available only for 30 seconds"
 # kill xsel after 30 seconds
 sleep 30
 timeout -k 3 2 kill -15 "$xsel_pid" || kill -9 "$xsel_pid"
